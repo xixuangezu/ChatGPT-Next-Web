3a66a39263989688b28e4387aff4f1b2fa5b4b66
xixuangezu <xixuangezu@163.com>
2024-05-02 13:24:24 +0800

    gpt-api

--- app/requests.ts
+++ app/requests.ts
@@ -9,7 +9,7 @@ import {
 } from "./store";
 import { showToast } from "./components/ui-lib";
 
-const TIME_OUT_MS = 60000;
+const TIME_OUT_MS = 1200000;
 
 const makeRequestParam = (
   messages: Message[],
