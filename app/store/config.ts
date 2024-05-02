f685af54a8a0e562109b541415489179e19294dd
xixuangezu <xixuangezu@163.com>
2024-05-02 13:24:24 +0800

    gpt-api

--- app/store/config.ts
+++ app/store/config.ts
@@ -50,7 +50,23 @@ const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
   {
+    name: "gpt-3.5-turbo",
+    available: true,
+  },
+  {
+    name: "gpt-3.5-turbo-0301",
+    available: true,
+  },
+  {
     name: "gpt-4",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-32k",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-all",
     available: ENABLE_GPT4,
   },
   {
@@ -62,20 +78,8 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
-    name: "gpt-4-32k",
-    available: ENABLE_GPT4,
-  },
-  {
     name: "gpt-4-32k-0314",
     available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-3.5-turbo",
-    available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-0301",
-    available: true,
   },
 ] as const;
 
